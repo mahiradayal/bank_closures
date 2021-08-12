2f7194a26b78808d3787670aefda1272de6d805a
mahiradayal <73258245+mahiradayal@users.noreply.github.com>
2021-08-12 13:49:56 -0400

    update map

--- visuals/louisiana_map/map.js
+++ visuals/louisiana_map/map.js
@@ -2,23 +2,43 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFoaXJhZGF5YWwiLCJhIjoiY2tzOHgxOHIyMDJzcTJub
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mahiradayal/cks8wvhzh0btf19qtbsk2vpoy',
-    zoom: 10,
-    center: [-93.722030, 32.461914]
+    zoom: 11,
+    center: [-93.7427, 32.4421]
 });
 
 map.on('load', function () {
     map.addLayer({
-        'id': 'caddoData',
+        'id': 'turnstileData',
         'type': 'circle',
         'source': {
             'type': 'geojson',
-            'data': 'data/data/caddo_bank_coords.geojson'
+            'data': 'data/caddo_bank_coord.geojson'
         },
         'paint': {
-            'circle-color': '#ff7f50',
+            'circle-color': '#14521D',
             'circle-stroke-color': '#4d4d4d',
-            'circle-stroke-width': 0.5,
-            'circle-radius': 5
+            'circle-stroke-width': 0.4,
+            'circle-radius': ['interpolate', ['linear'], ['get', 'ASSET'],
+                -1, 10,
+                -0.4, 1,
+            ]
         }
     });
+});
+
+map.on('click', 'turnstileData', function (e) {
+    var assets = e.features[0].properties.ASSET;
+    var name = e.features[0].properties.UNIT;
+    new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML("Asset Holding Size: $" + assets + '<br>' + "Bank Name: " + name)
+        .addTo(map);
+});
+// Change the cursor to a pointer when the mouse is over the turnstileData layer.
+map.on('mouseenter', 'turnstileData', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'turnstileData', function () {
+    map.getCanvas().style.cursor = '';
 });
